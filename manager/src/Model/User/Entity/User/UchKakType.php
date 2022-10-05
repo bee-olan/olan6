afc013318ac8721a0756e1a5fec1ec0bbbc28c66
olan <bee.olan@mail.ru>
2022-10-05 18:50:55 +0300

    спрограммировали свой тип данных

---
+++ manager/src/Model/User/Entity/User/UchKakType.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\Entity\User;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\StringType;
+
+class UchKakType extends StringType
+{
+    public const NAME = 'user_user_uchkak';
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return $value instanceof UchKak ? $value->getName() : $value;
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return !empty($value) ? new UchKak($value) : null;
+    }
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+}
