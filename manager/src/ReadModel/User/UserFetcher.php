d32325ee176ab00c85b8105d3445591d683753a1
olan <bee.olan@mail.ru>
2022-10-05 13:48:50 +0300

    начиная с урока 6

---
+++ manager/src/ReadModel/User/UserFetcher.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ReadModel\User;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\FetchMode;
+
+class UserFetcher
+{
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function existsByResetToken(string $token): bool
+    {
+        return $this->connection->createQueryBuilder()
+            ->select('COUNT (*)')
+            ->from('user_users')
+            ->where('reset_token_token = :token')
+            ->setParameter(':token', $token)
+            ->execute()->fetchColumn(0) > 0;
+    }
+
+    public function findForAuth(string $email): ?AuthView
+    {
+        $stmt = $this->connection->createQueryBuilder()
+            ->select(
+                'id',
+                'email',
+                'password_hash',
+                'role',
+                'status'
+            )
+            ->from('user_users')
+            ->where('email = :email')
+            ->setParameter(':email', $email)
+            ->execute();
+
+        $stmt->setFetchMode(FetchMode::CUSTOM_OBJECT, AuthView::class);
+        $result = $stmt->fetch();
+
+        return $result ?: null;
+    }
+
+    public function findByEmail(string $email): ?ShortView
+    {
+        $stmt = $this->connection->createQueryBuilder()
+            ->select(
+                'id',
+                'email',
+                'role',
+                'status'
+            )
+            ->from('user_users')
+            ->where('email = :email')
+            ->setParameter(':email', $email)
+            ->execute();
+
+        $stmt->setFetchMode(FetchMode::CUSTOM_OBJECT, ShortView::class);
+        $result = $stmt->fetch();
+
+        return $result ?: null;
+    }
+
+    public function findDetail(string $id): ?DetailView
+    {
+        $stmt = $this->connection->createQueryBuilder()
+            ->select(
+                'id',
+                'date',
+                'email',
+                'role',
+                'status'
+            )
+            ->from('user_users')
+            ->where('id = :id')
+            ->setParameter(':id', $id)
+            ->execute();
+
+        $stmt->setFetchMode(FetchMode::CUSTOM_OBJECT, DetailView::class);
+
+        /** @var DetailView $view */
+        $view = $stmt->fetch();
+
+        $stmt = $this->connection->createQueryBuilder()
+            ->select('network', 'identity')
+            ->from('user_user_networks')
+            ->where('user_id = :id')
+            ->setParameter(':id', $id)
+            ->execute();
+
+        $stmt->setFetchMode(FetchMode::CUSTOM_OBJECT, NetworkView::class);
+
+        $view->networks = $stmt->fetchAll();
+
+        return $view;
+    }
+}
---
+++ manager/translations/exceptions.ru.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'User is not found.' => '!!!!Пользователь не найден',
+
+    'Password is successfully changed.' => '!!!!Пароль успешно изменен.',
+    'Password resetting' => '!!!!Сброс пароля.',
+    'Incorrect or already confirmed token.'  => '!!! Неверный или уже подтвержденный токен.',
+
+    'Email is successfully confirmed.' => '!!!!! Электронная почта успешно подтверждена',
+    'Check your email.' => 'Проверь свою электронную почту',
+    'Unable to send message.' => 'Не удалось отправить сообщение.',
+];
