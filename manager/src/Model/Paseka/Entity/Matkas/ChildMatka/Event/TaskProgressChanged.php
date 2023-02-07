6bcee40fbcdab5050f2ac633f5b6e8d4ac78a10d
olan <bee.olan@mail.ru>
2023-02-07 20:46:17 +0300

    add начало 13-2 шаг 2

---
+++ manager/src/Model/Paseka/Entity/Matkas/ChildMatka/Event/TaskProgressChanged.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Paseka\Entity\Matkas\ChildMatka\Event;
+
+//use App\Model\Work\Entity\Members\Member\Id as MemberId;
+//use App\Model\Work\Entity\Projects\Task\Id;
+
+class TaskProgressChanged
+{
+    public $actorId;
+    public $taskId;
+    public $progress;
+
+    public function __construct(MemberId $actorId, Id $taskId, int $progress)
+    {
+        $this->actorId = $actorId;
+        $this->taskId = $taskId;
+        $this->progress = $progress;
+    }
+}
