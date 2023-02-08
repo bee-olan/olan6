170f754cf31bca3bad39fe3c3fb7e7323a89a2c9
olan <bee.olan@mail.ru>
2023-02-08 18:03:47 +0300

    add Передача URL-адреса websocket

--- manager/assets/js/app.js
+++ manager/assets/js/app.js
@@ -3,11 +3,11 @@ require('../css/app.scss');
 require('bootstrap');
 require('@coreui/coreui');
 
-
 const Centrifuge = require('centrifuge');
 
 document.addEventListener('DOMContentLoaded', function () {
-    const centrifuge = new Centrifuge('ws://localhost:8083/connection/websocket');
+    let url = document.querySelector('meta[name=centrifugo-url]').getAttribute('content');
+    let centrifuge = new Centrifuge(url);
     centrifuge.subscribe('alerts', function (message) {
         console.log(message.data.message);
     });
